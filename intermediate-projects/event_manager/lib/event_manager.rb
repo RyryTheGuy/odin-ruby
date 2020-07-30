ba70de654a1c1b3640ee9941f99da066a758b831
Ryan Roemer <ryanroemer61@gmail.com>
2020-07-30 13:00:20 -0500

    Added additional methods - Cleaned up attendee's phone numbers - Layed out the popular time's to register - Layed out the popular days to register

--- intermediate-projects/event_manager/lib/event_manager.rb
+++ intermediate-projects/event_manager/lib/event_manager.rb
@@ -1,6 +1,7 @@
 require "csv"
 require "google/apis/civicinfo_v2"
 require "erb"
+require "date"
 
 # Cleans up invalid zipcodes and returns valid zipcodes
 def clean_zipcode(zipcode)
@@ -13,6 +14,42 @@ def clean_zipcode(zipcode)
   # end
   
   zipcode.to_s.rjust(5, "0")[0..4]
+end
+
+def validate_phone_number(phone_number)
+  # Reformat all phone numbers to be the same
+  phone_number.gsub!(/\D/, "")
+  
+  if phone_number.length < 10 || phone_number.length > 11
+    return "Invalid Number"
+  elsif phone_number.length == 11
+    return phone_number[0] == "1" ? phone_number[1..10] : "Invalid Number"
+  end
+
+  phone_number
+end
+
+def get_hour_registered(time_string)
+  DateTime.strptime(time_string, '%m/%d/%y %H:%M').hour
+end
+
+def get_wday_registered(time_string)
+  case DateTime.strptime(time_string, '%m/%d/%y %H:%M').wday
+  when 0
+    "Sunday"
+  when 1
+    "Monday"
+  when 2
+    "Tuesday"
+  when 3
+    "Wednesday"
+  when 4
+    "Thursday"
+  when 5
+    "Friday"
+  when 6
+    "Saturday"
+  end
 end
 
 def legislators_by_zipcode(zip)
@@ -58,12 +95,18 @@ template_letter = File.read("form_letter.erb")
 # Creates an ERB object - ERB is a template language allowing actual Ruby code to be added to any text document
 erb_template = ERB.new(template_letter)
 
+popular_hours = Hash.new(0)
+popular_wday = Hash.new(0)
+
 # Loop through the event attendees. Every row is an attendee
 contents.each do |row|
   # Grab the attendee's id, name, zipcode, and array with their cities legislators (if any)
   id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
+  phone_number = validate_phone_number(row[:homephone])
+  popular_hours[get_hour_registered(row[:regdate])] += 1
+  popular_wday[get_wday_registered(row[:regdate])] += 1
   legislators = legislators_by_zipcode(zipcode)
 
   # Binding knows all the variables within the current scope. 
@@ -73,3 +116,6 @@ contents.each do |row|
 
   save_thank_you_letter(id, form_letter)
 end
+
+# p popular_wday
+# p popular_hours
